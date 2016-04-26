6f0f79c9bfc920b3128b04db87305419464fa80b
gwenf <gwenfaraday@gmail.com>
2016-04-26 09:39:10 -0400

    passport config continued...

--- client/components/Login.js
+++ client/components/Login.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
 import TopMenu from './TopMenu'
 import { Button, Col } from 'react-bootstrap'
+import { Form, FormGroup, FormControl, ControlLabel } from 'react-bootstrap'
+
+const margStyle = {
+  margin: '20px'
+}
 
 class Login extends Component {
   render(){
@@ -8,7 +13,36 @@ class Login extends Component {
       <div>
         <TopMenu />
         <Col xs={10} xsOffset={1}>
-          <h1>Login</h1>
+          <h1 style={margStyle}>Login</h1>
+        </Col>
+        <Col xs={5} xsOffset={1}>
+        <Form horizontal>
+          <FormGroup controlId="formHorizontalEmail">
+            <Col componentClass={ControlLabel} sm={2}>
+              Email
+            </Col>
+            <Col sm={10}>
+              <FormControl type="email" placeholder="Email" />
+            </Col>
+          </FormGroup>
+
+          <FormGroup controlId="formHorizontalPassword">
+            <Col componentClass={ControlLabel} sm={2}>
+              Password
+            </Col>
+            <Col sm={10}>
+              <FormControl type="password" placeholder="Password" />
+            </Col>
+          </FormGroup>
+
+          <FormGroup>
+            <Col smOffset={2} sm={10}>
+              <Button type="submit" bsStyle="primary">
+                Sign in
+              </Button>
+            </Col>
+          </FormGroup>
+        </Form>
         </Col>
       </div>
     )
--- client/components/Register.js
+++ client/components/Register.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
 import TopMenu from './TopMenu'
 import { Jumbotron, Button, Col } from 'react-bootstrap'
+import { Form, FormGroup, FormControl, ControlLabel } from 'react-bootstrap'
+
+
+const margStyle = {
+  margin: '20px'
+}
 
 class Register extends Component {
   render(){
@@ -8,7 +14,54 @@ class Register extends Component {
       <div>
         <TopMenu />
         <Col xs={10} xsOffset={1}>
-          <h1>Sign Up</h1>
+          <h1 style={margStyle}>Sign Up</h1>
+        </Col>
+        <Col xs={5} xsOffset={1}>
+        <Form horizontal>
+          <FormGroup controlId="formHorizontalEmail">
+            <Col componentClass={ControlLabel} sm={2}>
+              Name
+            </Col>
+            <Col sm={10}>
+              <FormControl type="text" placeholder="First Name" />
+            </Col>
+          </FormGroup>
+
+          <FormGroup controlId="formHorizontalEmail">
+            <Col componentClass={ControlLabel} sm={2}>
+              Username
+            </Col>
+            <Col sm={10}>
+              <FormControl type="text" placeholder="Username" />
+            </Col>
+          </FormGroup>
+
+          <FormGroup controlId="formHorizontalEmail">
+            <Col componentClass={ControlLabel} sm={2}>
+              Email
+            </Col>
+            <Col sm={10}>
+              <FormControl type="email" placeholder="Email" />
+            </Col>
+          </FormGroup>
+
+          <FormGroup controlId="formHorizontalPassword">
+            <Col componentClass={ControlLabel} sm={2}>
+              Password
+            </Col>
+            <Col sm={10}>
+              <FormControl type="password" placeholder="Password" />
+            </Col>
+          </FormGroup>
+
+          <FormGroup>
+            <Col smOffset={2} sm={10}>
+              <Button type="submit" bsStyle="primary">
+                Sign in
+              </Button>
+            </Col>
+          </FormGroup>
+        </Form>
         </Col>
       </div>
     )
