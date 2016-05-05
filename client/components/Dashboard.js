954dd47f95b1bba7e71a7a15425f8253eb9971bd
gwenf <gwenfaraday@gmail.com>
2016-05-04 20:21:22 -0400

    fixed es6 syntax for ajax request

--- client/components/Dashboard.js
+++ client/components/Dashboard.js
@@ -5,9 +5,13 @@ import UserSidebar from './Sidebar/UserSidebar'
 import $ from 'jquery'
 
 class Dashboard extends Component {
-  getInitialState() {
-    return {name: ''};
-  }
+  // getInitialState() {
+  //   return {name: ''};
+  // }
+  constructor() {
+    super();
+    this.state = {name: ''};
+   }
   componentDidMount() {
     this.serverRequest = $.get('/data', function (result) {
       console.log(result.data.name)
@@ -21,7 +25,7 @@ class Dashboard extends Component {
       <div>
         <TopMenuLoggedIn />
         <Col xs={7} xsOffset={1}>
-          <h1>{name} Dashboard</h1>
+          <h1>{this.state.name} Dashboard</h1>
         </Col>
         <Col xs={3}>
           <UserSidebar />
